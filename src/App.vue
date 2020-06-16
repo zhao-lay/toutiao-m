2b3c9767d40fd8c55403c910238e8f4e5b5801a0
goodluck <zhao.lay@qq.com>
2020-06-16 18:54:40 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao iconshipin"></i>
+    </div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <!-- <router-view/> 是路由的出口 没了就看不见路由界面了-->
     <router-view/>
   </div>
